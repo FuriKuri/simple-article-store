d26a281d08dae446e2107fb2691e35e26498a313
Theo Pack Test <tf.pack@gmail.com>
2017-02-10 20:52:55 +0100

    Add controller

--- src/main/java/net/furikuri/controller/ArticleController.java
+++ src/main/java/net/furikuri/controller/ArticleController.java
@@ -1,5 +1,28 @@
 package net.furikuri.controller;
 
 
+import net.furikuri.domain.Article;
+import net.furikuri.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class ArticleController {
+
+    @Autowired private ArticleService articleService;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("articles", articleService.all());
+        model.addAttribute("article", new Article());
+        return "index";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String addTodo(@ModelAttribute Article article) {
+        articleService.add(article);
+        return "redirect:/";
+    }
 }
