f680e1202cbf33a74867f3acad2300a59ed43a0f
Theo Pack Test <tf.pack@gmail.com>
2017-02-10 21:18:11 +0100

    Add endpoint for json response

--- src/main/java/net/furikuri/controller/ArticleController.java
+++ src/main/java/net/furikuri/controller/ArticleController.java
@@ -4,14 +4,26 @@ package net.furikuri.controller;
 import net.furikuri.domain.Article;
 import net.furikuri.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@Controller
 public class ArticleController {
 
     @Autowired private ArticleService articleService;
+
+    @RequestMapping("/json")
+    @ResponseBody
+    public List<Article> all() {
+        return articleService.all();
+    }
 
     @RequestMapping("/")
     public String index(Model model) {
